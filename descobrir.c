1158da07fefcd8845de93da72bceaed26d46ee39
KaianeSousa <132933423+KaianeSousa@users.noreply.github.com>
2023-11-18 19:35:56 -0300

    Update descobrir.c
    adição de comentários

--- descobrir.c
+++ descobrir.c
@@ -1,4 +1,4 @@
-#include "blb.h"
+#include "blb.h" //Inclui o arquivo da biblioteca "blb.h"
 
 void interageOperacoes(){
 
@@ -14,9 +14,10 @@ void interageOperacoes(){
         scanf("%d", &opcao);
 
         switch(opcao){
+            
             int resultado = 0;
 
-            case 1:
+            case 1: //Calcula o fatorial de um número digitado pelo usuário
             {
                 int numero;
                 
@@ -29,12 +30,11 @@ void interageOperacoes(){
             }
             break;
 
-
-            case 2:
+            case 2: //Calcula o somatório de 1 até o número digitado pelo usuário
             {
                 int n;
 
-                printf("Digite um número para calcular o somatório de 1 até n: ");
+                printf("Digite um número para calcular o somatório de 1 até %d: ", n);
                 scanf("%d", &n);
                 
                 resultado = somatorio(n);
@@ -43,7 +43,7 @@ void interageOperacoes(){
             }
             break;
 
-            case 3:
+            case 3: //Calcula n termo da sequência de Fibonacci
             {
                 int termo;
                 
@@ -57,15 +57,16 @@ void interageOperacoes(){
             }
             break;
 
-            case 0: {
+            case 0: //Encerra essa função e volta para a anterior
+            {
 
-            printf("Saindo do programa.\n");
-            return;
+                printf("Saindo do programa.\n");
+                return;
              }
                 break;
 
             default:
-            printf("Erro! Por favor, escolha uma das opções fornecidadas.\n");
+                printf("Erro! Por favor, escolha uma das opções fornecidadas.\n");
 
             }
 
@@ -73,21 +74,7 @@ void interageOperacoes(){
 
 }
 
-
-// Função para calcular o somatório de 1 até n
-int somatorio(int n){
-
-    if (n == 0)
-    {
-        return 0;
-
-    }else{
-
-        return n + somatorio(n - 1);
-    }
-}
-
-// Função para calcular o fatorial de um número
+//Função para calcular o fatorial de um número
 int fatorial(int n){
 
     if(n == 0 || n == 1)
@@ -100,7 +87,20 @@ int fatorial(int n){
     }
 }
 
-// Função para calcular o n-ésimo termo da sequência de Fibonacci
+//Função para calcular o somatório de 1 até n
+int somatorio(int n){
+
+    if (n == 0)
+    {
+        return 0;
+
+    }else{
+
+        return n + somatorio(n - 1);
+    }
+}
+
+//Função para calcular o n-ésimo termo da sequência de Fibonacci
 int fibonacci(int n){
 
     if (n == 0)
@@ -115,5 +115,6 @@ int fibonacci(int n){
         return fibonacci(n - 1) + fibonacci(n - 2);
     }
 }
+
 
 
