1eebf4687178a02ca23f58d14e02507da612ab6c
KaianeSousa <132933423+KaianeSousa@users.noreply.github.com>
2023-11-18 19:32:10 -0300

    Update busca.c
    adição dos comentários

--- busca.c
+++ busca.c
@@ -1,13 +1,14 @@
-#include "blb.h"
+#include "blb.h" //Inclui o arquivo da biblioteca "blb.h"
 
+//Função de interação para buscar um determinado número em uma determinada ordem
 void interageBusca(){
     
-    int num = 0;
-    char nomeArquivo[] = "numeros.txt";
-    int numeros[100];
-    int quantidade;
+    int num = 0; //Variável para leitura de um valor inserido pelo usuário
+    char nomeArquivo[] = "numeros.txt"; //Nome do arquivo a ser lido
+    int numeros[100]; //Array que armazenará os números a serem ordenados
+    int quantidade; //Variável para armazenar a quantidade de números lidos
     FILE *arquivo = NULL; //Declaração da variável arquivo
-    int opcao;
+    int opcao; //Variável para a leitura da escolha do usuário
 
 
       do{
@@ -22,8 +23,8 @@ void interageBusca(){
 
          switch (opcao)
         {
-            case 1: {
-
+            case 1: //Busca Linear 
+            { 
                 printf("Digite o número que deseja buscar: ");
                 scanf("%d", &num);
                 
@@ -39,7 +40,7 @@ void interageBusca(){
             }
             break;
 
-            case 2:
+            case 2: //Busca Sentinela
             {
 
                 printf("Digite o número que deseja buscar: ");
@@ -58,7 +59,7 @@ void interageBusca(){
             }
             break;
 
-            case 3:
+            case 3: //Busca Binária Recursiva
             {
 
             printf("Digite o número que deseja buscar: ");
@@ -77,7 +78,7 @@ void interageBusca(){
         }
             break;
 
-            case 0: 
+            case 0: //Encerra essa função e volta para a anterior
             {
                      printf("Saindo do programa.\n");
                      return;
@@ -93,6 +94,7 @@ void interageBusca(){
 
 }
 
+//Função de busca linear em um array
 int buscaLinear(int *numeros, int quantidade, int num){
 
     for (int i = 0; i < quantidade; i++)
@@ -103,7 +105,7 @@ int buscaLinear(int *numeros, int quantidade, int num){
 
     return -1;
 }
-
+//Função de busca sentinela em um array
 int buscaSentinela(int *numeros, int quantidade, int num)
 {
     int last = numeros[quantidade - 1];
@@ -130,7 +132,7 @@ int buscaSentinela(int *numeros, int quantidade, int num)
     }
 }
 
-
+//Função de busca binária recursiva em um array ordenado
 int buscaBinariaRecursiva(int *numeros, int esquerda, int direita, int num)
 {
     if (esquerda <= direita)
@@ -154,5 +156,6 @@ int buscaBinariaRecursiva(int *numeros, int esquerda, int direita, int num)
 
     return -1;
 }
+
 
 
